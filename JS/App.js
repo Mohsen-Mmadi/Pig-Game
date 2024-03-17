4fb87ac896ebff02ad3c11b0a0e410356fdd772e
Mohsen_Mohammadi <mohsen.mohammadii3446@gmail.com>
2024-03-17 22:49:06 +0330

    Moving players and collecting points in the temporary score

--- JS/App.js
+++ JS/App.js
@@ -1,10 +1,24 @@
 let btnDice=document.getElementById('btn-dice')
 let imageDiceRandom=document.getElementsByClassName('img-dice')[0]
+let temporaryNumber1=document.getElementById('temporary-number1')
+let temporaryNumber2=document.getElementById('temporary-number2')
 
 // function
-
+let score=0 
+let activePlayer=1
     btnDice.addEventListener('click', ()=>{
-    let randomImg=Math.floor(Math.random()*6+1)
-    imageDiceRandom.src=`../image/${randomImg}.jpg`
+    let randomnumber=Math.floor(Math.random()*6+1)
+    imageDiceRandom.src=`../image/${randomnumber}.jpg`
     imageDiceRandom.classList.remove('d-none')
+    
+    if(randomnumber!=1){
+        score+=randomnumber
+        document.getElementById(`temporary-number${activePlayer}`).textContent=score
+    }else{
+        score=0
+        randomnumber=0
+        document.getElementById(`temporary-number${activePlayer}`).textContent=0
+        score+=randomnumber
+        activePlayer=activePlayer===1?2:1;
+    }
 })

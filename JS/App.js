f0216c2ffca865c23a720603691a69004a71901e
Mohsen_Mohammadi <mohsen.mohammadii3446@gmail.com>
2024-03-18 00:53:31 +0330

    checge background-color for users

--- JS/App.js
+++ JS/App.js
@@ -1,24 +1,36 @@
-let btnDice=document.getElementById('btn-dice')
-let imageDiceRandom=document.getElementsByClassName('img-dice')[0]
-let temporaryNumber1=document.getElementById('temporary-number1')
-let temporaryNumber2=document.getElementById('temporary-number2')
-
+let btnDice = document.getElementById("btn-dice");
+let imageDiceRandom = document.getElementsByClassName("img-dice")[0];
+let temporaryNumber1 = document.getElementById("temporary-number1");
+let temporaryNumber2 = document.getElementById("temporary-number2");
+let bgUser1 = document.getElementById("backgrund-user1");
+let bgUser2 = document.getElementById("backgrund-user2");
 // function
-let score=0 
-let activePlayer=1
-    btnDice.addEventListener('click', ()=>{
-    let randomnumber=Math.floor(Math.random()*6+1)
-    imageDiceRandom.src=`../image/${randomnumber}.jpg`
-    imageDiceRandom.classList.remove('d-none')
-    
-    if(randomnumber!=1){
-        score+=randomnumber
-        document.getElementById(`temporary-number${activePlayer}`).textContent=score
-    }else{
-        score=0
-        randomnumber=0
-        document.getElementById(`temporary-number${activePlayer}`).textContent=0
-        score+=randomnumber
-        activePlayer=activePlayer===1?2:1;
+let score = 0;
+let activePlayer = 1;
+btnDice.addEventListener("click", () => {
+  let randomnumber = Math.floor(Math.random() * 6 + 1);
+  imageDiceRandom.src = `../image/${randomnumber}.jpg`;
+  imageDiceRandom.classList.remove("d-none");
+
+  if (randomnumber != 1) {
+    score += randomnumber;
+    document.getElementById(`temporary-number${activePlayer}`).textContent =
+      score;
+  } else {
+    score = 0;
+    randomnumber = 0;
+    document.getElementById(`temporary-number${activePlayer}`).textContent = 0;
+    score += randomnumber;
+    if (activePlayer === 1) {
+      activePlayer = activePlayer = 2;
+      document.getElementById("backgrund-user1").classList.toggle("bg-active");
+      document.getElementById("backgrund-user2").classList.remove('bg-user')
+      document.getElementById("backgrund-user2").classList.toggle("bg-active");
+    } else {
+      activePlayer = activePlayer = 1;
+      document.getElementById("backgrund-user1").classList.toggle("bg-active");
+      document.getElementById("backgrund-user2").classList.remove('bg-user')
+      document.getElementById("backgrund-user2").classList.toggle("bg-active");
     }
-})
+  }
+});
